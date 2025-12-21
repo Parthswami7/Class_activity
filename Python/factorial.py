027272f30868ad8287a548ca8b6d123252cbd15c
Parthswami7 <swamimanisha039@gmail.com>
2025-12-21 22:40:25 +0530

    Add files via upload

---
+++ Python/factorial.py
@@ -0,0 +1,23 @@
+def add(x,y):
+    return x+y
+
+def sub(x,y):
+    return x-y
+
+def mul(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
+
+num1 = int(input("Enter a number:"))
+num2 = int(input("Enter another number:"))
+
+
+print(add(num1,num2))
+
+print(sub(num1,num2))
+
+print(mul(num1,num2))
+
+print(divide(num1,num2))
