3d724ecd2c0b8b89994d75425b8154c63a20f4ad
Parth <swamimanisha039@gmail.com>
2026-08-14 21:40:26 +0530

    Statistics

---
+++ Statistics/Statistics6.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+import numpy as np 
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+data = sns.load_dataset('iris')
+categories = ['setosa','virginica','versicolor']
+data['flower_num'] = pd.Categorical(data['species'],categories, ordered=True)
+median_value = np.median(data['flower_num'].cat.codes)
+median_text = categories[int(median_value)]
+print("Meidan Value of Species -", median_text)
---
+++ Statistics/Statistics7.py
@@ -0,0 +1,21 @@
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+data = pd.read_csv('Titanic Dataset.csv')
+data.head(5)
+data.dtypes
+nominal_cat = ['Name','Ticket','Cabin']
+ordinal_cat = ['Embarked','Gender']
+data['Gender'].value_counts()
+gender_categories = ['Female','Male']
+data['Gender'] = pd.Categorical(data['Gender'],gender_categories,ordered=True)
+median_index = np.median(data['Gender'].cat.codes)
+median_gender = gender_categories[int(median_index)]
+print(median_gender)
+data['Embarked'].value_counts()
+embarked_categories = ['S','C','Q']
+data['Embarked'] = pd.Categorical(data['Embarked'],embarked_categories,ordered=True)
+median_index = np.median(data['Embarked'].cat.codes)
+median_embarked = embarked_categories[int(median_index)]
+print(median_embarked)
